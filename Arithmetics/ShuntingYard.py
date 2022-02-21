775e3f4cd811e7946d5d087dba05056039fe88ad
Robert Larsen <larsen.rcl@gmail.com>
2022-02-20 22:32:48 -0600

    Adds Arithmetics kata folder and initial implementation of Shunting Yard algorithm

---
+++ Arithmetics/ShuntingYard.py
@@ -0,0 +1,77 @@
+from collections import namedtuple
+from pprint import pprint as pp
+
+NUM = 'NUMBER'
+LEFTPARENTHESES = '('
+RIGHTPARENTHESES = ')'
+LEFT = 'Left'
+RIGHT = 'Right'
+
+class ShuntingYard():
+    def __init__(self) -> None:
+        self.operatorInfo = namedtuple('OperatorInfo', 'precedence associativeness')
+    
+        self.ops = {
+         '^': self.operatorInfo(precedence=4, associativeness=RIGHT),
+         '*': self.operatorInfo(precedence=3, associativeness=LEFT),
+         '/': self.operatorInfo(precedence=3, associativeness=LEFT),
+         '+': self.operatorInfo(precedence=2, associativeness=LEFT),
+         '-': self.operatorInfo(precedence=2, associativeness=LEFT),
+         '(': self.operatorInfo(precedence=9, associativeness=LEFT),
+         ')': self.operatorInfo(precedence=0, associativeness=LEFT),
+         }
+
+    def tokenizeInput(self, inputString = None):    
+        if inputString is None:
+            inputString = input('expression: ')
+
+        tokens = self.__splitString(inputString)
+        return self.__createTokenMap(tokens)
+    
+    #TODO: Revisit splitting this into functions. Everything I've tried so far has been terrible.
+    def doShuntingYard(self, tokenMap):
+        rpnResult, rpnStack = [], []
+        for token, value in tokenMap:
+            if token is NUM:
+                rpnResult.append(value)
+            elif token in self.ops:
+                currentToken, (currentPrecedence, currentAssociativeness) = token, value
+                while rpnStack:
+                    previousToken, (previousPrecendence, previousAssociativeness) = rpnStack[-1]
+                    if (currentAssociativeness == LEFT and currentPrecedence <= previousPrecendence) or (currentAssociativeness == RIGHT and currentPrecedence < previousPrecendence):
+                        if currentToken != RIGHTPARENTHESES:
+                            if previousToken != LEFTPARENTHESES:
+                                rpnStack.pop()
+                                rpnResult.append(previousToken)
+                            else:    
+                                break
+                        else:        
+                            if previousToken != LEFTPARENTHESES:
+                                rpnStack.pop()
+                                rpnResult.append(previousToken)
+                            else:    
+                                rpnStack.pop()
+                                break
+                    else:
+                        break
+                if currentToken != RIGHTPARENTHESES:
+                    rpnStack.append((token, value))
+
+        while rpnStack:
+            previousToken, (previousPrecendence, previousAssociativeness) = rpnStack[-1]
+            rpnStack.pop()
+            rpnResult.append(previousToken)
+        return rpnResult
+
+
+    def __splitString(self, string):
+        return string.strip().split()
+
+    def __createTokenMap(self, tokens):
+        tokenMap = []
+        for token in tokens:
+            if token in self.ops:
+                tokenMap.append((token, self.ops[token]))
+            else:    
+                tokenMap.append((NUM, token))
+        return tokenMap
---
+++ Arithmetics/ShuntingYardTest.py
@@ -0,0 +1,17 @@
+
+from Shunt import *
+
+shunt = ShuntingYard()
+
+if __name__ == '__main__':
+    infix = '( 5 * ( -4 * ( 3 * ( 2 * ( 1 * 9 ) / 8 - 7 ) + 6 ) ) )'
+    print( 'For infix expression: %r\n' % infix )
+    rp = shunt.doShuntingYard(shunt.tokenizeInput(infix))
+    # maxcolwidths = [len(max(x, key=len)) for x in zip(*rp)]
+    # row = rp[0]
+    # print( ' '.join('{cell:^{width}}'.format(width=width, cell=cell) for (width, cell) in zip(maxcolwidths, row)))
+    # for row in rp[1:]:
+        # print( ' '.join('{cell:<{width}}'.format(width=width, cell=cell) for (width, cell) in zip(maxcolwidths, row)))
+ 
+    #print('\n The final output RPN is: %r' % rp[-1][2])
+    print(*rp)
