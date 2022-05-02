871b237f2aa393ebc49ececb9694e359d50ae712
Robert Larsen <larsen.rcl@gmail.com>
2022-05-01 21:44:48 -0500

    Implement FizzBuzzChecker

--- FizzBuzz/FizzBuzzChecker.py
+++ FizzBuzz/FizzBuzzChecker.py
@@ -3,4 +3,11 @@ class FizzBuzzChecker():
         pass
 
     def GetFizzBuzzResult(self, value):
-        pass
+        if (value%3 == 0) and not (value%5 == 0):
+            return "Fizz"
+        if (not value%3 == 0) and (value%5 == 0):
+            return "Buzz"
+        if (value %3 == 0) and (value%5 == 0):
+            return "FizzBuzz"
+        
+        return str(value)
