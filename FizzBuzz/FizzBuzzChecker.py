7d0ad6b7e63ee308db603a4b4528ccccd3afa86d
Robert Larsen <larsen.rcl@gmail.com>
2022-05-01 21:51:45 -0500

    Clean GetFizzBuzzResult implementation

--- FizzBuzz/FizzBuzzChecker.py
+++ FizzBuzz/FizzBuzzChecker.py
@@ -3,11 +3,24 @@ class FizzBuzzChecker():
         pass
 
     def GetFizzBuzzResult(self, value):
-        if (value%3 == 0) and not (value%5 == 0):
-            return "Fizz"
-        if (not value%3 == 0) and (value%5 == 0):
-            return "Buzz"
-        if (value %3 == 0) and (value%5 == 0):
-            return "FizzBuzz"
+        result = str(value)
+        if self._isFizz(value):
+            result = "Fizz"
+        elif self._isBuzz(value):
+            result = "Buzz"
+        elif self._isFizzBuzz(value):
+            result = "FizzBuzz"
         
-        return str(value)
+        return result
+
+    def _isFizz(self, value):
+        if (value%3 == 0) and not (value%5 == 0):
+            return True
+
+    def _isBuzz(self, value):
+        if (not value%3 == 0) and (value%5 == 0):
+            return True
+
+    def _isFizzBuzz(self, value):
+        if (value %3 == 0) and (value%5 == 0):
+            return True
