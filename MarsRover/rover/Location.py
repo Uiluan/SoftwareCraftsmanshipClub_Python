2734afe18a43e9cb9ba925ae778ce50ce0a78ef4
Robert Larsen <larsen.rcl@gmail.com>
2022-05-10 22:38:02 -0500

    Add Location class to abstract Direction and Coordinate

---
+++ MarsRover/rover/Location.py
@@ -0,0 +1,32 @@
+from rover.Direction import Direction
+from rover.Coordinate import Coordinate
+import rover.Constants as const
+
+class Location():
+    def __init__(self, x:int, y:int, direction:int) -> None:
+        self.direction = Direction(direction)
+        self.coordinate = Coordinate(x, y)
+
+    def TurnLeft(self):
+        self.direction.TurnLeft()
+
+    def TurnRight(self):
+        self.direction.TurnRight()
+
+    def Move(self):
+        if self.direction.IsNorth():
+            self.coordinate.MoveNorth()
+        elif self.direction.IsWest():
+            self.coordinate.MoveWest()
+        elif self.direction.IsSouth():
+            self.coordinate.MoveSouth()
+        elif self.direction.IsEast():
+            self.coordinate.MoveEast()
+
+    def GetLocation(self):
+        x, y = self.coordinate.GetCoordinates()
+        dir = self.direction.GetDirection()
+        return [x, y, dir]
+
+    def GetString(self):
+        return self.coordinate.GetString() + ":" + self.direction.GetString()
---
+++ MarsRover/tests/test_Location.py
@@ -0,0 +1,124 @@
+import rover.Constants as const
+from rover.Location import Location
+
+def test_TurnLeft_GivenInitial_00N_EndsAt_00W():
+    location = Location(0, 0, const.consts.NORTH)
+
+    location.TurnLeft()
+
+    expected = [0, 0, const.consts.WEST]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_TurnLeft_GivenInitial_00W_EndsAt_00S():
+    location = Location(0, 0, const.consts.WEST)
+
+    location.TurnLeft()
+
+    expected = [0, 0, const.consts.SOUTH]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_TurnLeft_GivenInitial_00S_EndsAt_00E():
+    location = Location(0, 0, const.consts.SOUTH)
+
+    location.TurnLeft()
+
+    expected = [0, 0, const.consts.EAST]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_TurnLeft_GivenInitial_00E_EndsAt_00N():
+    location = Location(0, 0, const.consts.EAST)
+
+    location.TurnLeft()
+
+    expected = [0, 0, const.consts.NORTH]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_TurnRight_GivenInitial_00N_EndsAt_00E():
+    location = Location(0, 0, const.consts.NORTH)
+
+    location.TurnRight()
+
+    expected = [0, 0, const.consts.EAST]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_TurnLeft_GivenInitial_00W_EndsAt_00N():
+    location = Location(0, 0, const.consts.WEST)
+
+    location.TurnRight()
+
+    expected = [0, 0, const.consts.NORTH]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_TurnLeft_GivenInitial_00S_EndsAt_00W():
+    location = Location(0, 0, const.consts.SOUTH)
+
+    location.TurnRight()
+
+    expected = [0, 0, const.consts.WEST]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_TurnLeft_GivenInitial_00E_EndsAt_00S():
+    location = Location(0, 0, const.consts.EAST)
+
+    location.TurnRight()
+
+    expected = [0, 0, const.consts.SOUTH]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_GetLocation_GivenLocation_87E_Returns_87E():
+    location = Location(8, 7, const.consts.EAST)
+
+    expected = [8, 7, const.consts.EAST]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_GetString_GivenLocation_42W_Returns_42W_String():
+    location = Location(4, 2, const.consts.WEST)
+
+    expected = "4:2:W"
+    actual = location.GetString()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_Move_Given_55N_EndsAt_56N():
+    location = Location(5, 5, const.consts.NORTH)
+
+    location.Move()
+
+    expected = [5, 6, const.consts.NORTH]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_Move_Given_55W_EndsAt_56W():
+    location = Location(5, 5, const.consts.WEST)
+
+    location.Move()
+
+    expected = [4, 5, const.consts.WEST]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_Move_Given_55S_EndsAt_56S():
+    location = Location(5, 5, const.consts.SOUTH)
+
+    location.Move()
+
+    expected = [5, 4, const.consts.SOUTH]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
+
+def test_Move_Given_55E_EndsAt_56E():
+    location = Location(5, 5, const.consts.EAST)
+
+    location.Move()
+
+    expected = [6, 5, const.consts.EAST]
+    actual = location.GetLocation()
+    assert actual == expected, f"Expected {expected}, got: {actual}"
